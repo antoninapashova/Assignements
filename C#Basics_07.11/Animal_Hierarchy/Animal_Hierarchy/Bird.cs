35178489dcde6feb4a4bf107f984c8e051889b6a
antoninapashova <antonina_pashova@abv.bg>
2022-11-07 15:02:46 +0200

    Add animal hierarchy

---
+++ C#Basics_07.11/Animal_Hierarchy/Animal_Hierarchy/Bird.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Hierarchy
+{
+    public class Bird : Animal
+    {
+        public int meters { get; set; }
+        public Bird(string name, int age) : base(name, age)
+        {
+
+        }
+
+        public override void move()
+        {
+            Console.WriteLine("The bird is flying");
+        }
+
+        public override void move(int meters)
+        {
+            this.meters += meters;
+            Console.WriteLine("The bird flied {0} meters", meters);
+            Console.WriteLine("Total meters "+ meters);
+        }
+    }
+}
---
+++ C#Basics_07.11/Animal_Hierarchy/Animal_Hierarchy/Mammal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Hierarchy
+{
+    public class Mammal : Animal
+
+    {
+        public int steps { get; set; }
+        public Mammal(string name, int age) : base(name, age)
+        {
+        }
+
+        /*
+        public override void eat()
+        {
+            Console.WriteLine("The animal is eating");
+        }
+        */
+        public override void move()
+        {
+           Console.WriteLine("The mamamal is walking");
+        }
+
+        public override void move(int steps)
+        {
+            this.steps += steps;
+            Console.WriteLine("The mammal took {0} steps", steps);
+            Console.WriteLine("Total steps: " + this.steps);
+        }
+    }
+}
