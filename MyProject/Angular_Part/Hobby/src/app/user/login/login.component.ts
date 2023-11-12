b096dad9e882202e4d8795039eccae2a72381848
antoninapashova <95548323+antoninapashova@users.noreply.github.com>
2023-11-12 17:27:09 +0200

    Format login component

--- MyProject/Angular_Part/Hobby/src/app/user/login/login.component.ts
+++ MyProject/Angular_Part/Hobby/src/app/user/login/login.component.ts
@@ -5,7 +5,7 @@ import { ModalType } from './../../core/dialog/dialog-template/dialog-template.c
 import { MatDialog } from '@angular/material/dialog';
 import { UserService } from './../user.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,  Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { DialogTemplateComponent } from 'src/app/core/dialog/dialog-template/dialog-template.component';
@@ -17,29 +17,29 @@ import { IUser } from 'src/app/shared/interfaces/user';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-   
+
   invalidLogin?: boolean;
-  hide : boolean = true;
+  hide: boolean = true;
   user!: IUser;
   loginUserForm: FormGroup = new FormGroup({});
 
   constructor(private formBuilder: FormBuilder, private router: Router,
-              private userService: UserService,private jwtHelper : JwtHelperService,
-              private matDialog: MatDialog, private userStoreService: UserStoreService, 
-              private dataSharingService: DataSharingService
-           ) { }
+    private userService: UserService, private jwtHelper: JwtHelperService,
+    private matDialog: MatDialog, private userStoreService: UserStoreService,
+    private dataSharingService: DataSharingService
+  ) { }
 
   ngOnInit(): void {
-       this.loginUserForm = this.formBuilder.group({
-         username: [null, [Validators.required, Validators.minLength(3)]],
-         password: [null, [Validators.required, Validators.minLength(5)]],
-       });
+    this.loginUserForm = this.formBuilder.group({
+      username: [null, [Validators.required, Validators.minLength(3)]],
+      password: [null, [Validators.required, Validators.minLength(5)]],
+    });
   }
-  
-  onSubmit (form: FormGroup) {
-     if(form.valid){        
-       this.userService.login(form.value).subscribe({
-        next:(res)=>{
+
+  onSubmit(form: FormGroup) {
+    if (form.valid) {
+      this.userService.login(form.value).subscribe({
+        next: (res) => {
           this.user = res;
           this.dataSharingService.loggedInUser = this.user;
           this.userService.storeToken(res.accessToken);
@@ -50,23 +50,23 @@ export class LoginComponent implements OnInit {
           this.router.navigate(['home']);
           this.dataSharingService.isUserLoggedIn.next(true);
         },
-        error:(err)=>{
-          let obj ={title: 'Login', message: err, type: ModalType.WARN}
-          this.matDialog.open( DialogTemplateComponent, {data: obj})
+        error: (err) => {
+          let obj = { title: 'Login', message: err, type: ModalType.WARN };
+          this.matDialog.open(DialogTemplateComponent, { data: obj });
         }
-       });
-     }
-   }
+      });
+    }
+  }
 
   isUserAuthenticated() {
     const token = localStorage.getItem("jwt");
     if (token && !this.jwtHelper.isTokenExpired(token)) {
       return true;
     }
-    else {return false;}
-  } 
-  
-  openDialog(){
+    else { return false; }
+  }
+
+  openDialog() {
     this.matDialog.open(ForgetPasswordComponent)
   }
 }
