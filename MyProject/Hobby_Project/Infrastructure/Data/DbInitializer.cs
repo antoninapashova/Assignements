a79cbc9f51f49ff42b73dac30cfee4a8ff50bd58
antoninapashova <95548323+antoninapashova@users.noreply.github.com>
2023-11-12 22:10:52 +0200

    Implement DbInitializer to seed default user

--- MyProject/Hobby_Project/Infrastructure/Data/DbInitializer.cs
+++ MyProject/Hobby_Project/Infrastructure/Data/DbInitializer.cs
@@ -3,7 +3,6 @@ using HobbyProject.Domain.Entity;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace HobbyProject.Infrastructure.Data
 {
@@ -21,20 +20,23 @@ namespace HobbyProject.Infrastructure.Data
 
         private static void InitAdmin(HobbyDbContext context)
         {
-            var user = new UserEntity 
-            { 
-                Username = "Admin", 
-                FirstName = "Antonina", 
-                LastName = "Pashova", 
-                Email = "antoninapasova95@gmail.com", 
-                Role = "Admin",
-                Password = PasswordHasher.HashPassword("123456"),
-                Token = "",
-                RefreshToken = ""
-            };
+            if (!context.Users.Any()) {
 
-            context.Users.Add(user);
-            context.SaveChanges();  
+                var user = new UserEntity 
+                { 
+                    Username = "Admin", 
+                    FirstName = "Antonina", 
+                    LastName = "Pashova", 
+                    Email = "antoninapasova95@gmail.com", 
+                    Role = "Admin",
+                    Password = PasswordHasher.HashPassword("123456"),
+                    Token = "",
+                    RefreshToken = ""
+                };
+
+                context.Users.Add(user);
+                context.SaveChanges();  
+            }
         }
     }
 }
