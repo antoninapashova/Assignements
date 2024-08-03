f7b81583aec31dcb63bc909c53920d553d5b1fcb
antoninapashova <95548323+antoninapashova@users.noreply.github.com>
2024-08-03 13:30:17 +0300

    Change validation for SubCategory

--- MyProject/Hobby_Project/Application/Validators/SubCategoryValidator.cs
+++ MyProject/Hobby_Project/Application/Validators/SubCategoryValidator.cs
@@ -7,7 +7,7 @@ namespace HobbyProject.Application.Validators
      {
         public SubCategoryValidator()
         {
-            RuleFor(x => x.Name).NotNull().NotEmpty().Length(3, 10)
+            RuleFor(x => x.Name).NotNull().NotEmpty().Length(3, 20)
                 .WithMessage("SubCategory name must be between 3 and 10 characters!");
         }
      }
