5407e802eb0781a90d234d331939e5b0169ef229
antoninapashova <95548323+antoninapashova@users.noreply.github.com>
2024-09-14 14:13:05 +0300

    Refactor design of user card

--- MyProject/Angular_Part/Hobby/src/app/core/user-card/user-card.component.ts
+++ MyProject/Angular_Part/Hobby/src/app/core/user-card/user-card.component.ts
@@ -1,6 +1,9 @@
-
 import { Component, OnInit } from '@angular/core';
 import { IUser } from 'src/app/shared/interfaces/user';
+import { UserService } from 'src/app/user/user.service';
+import { DataSharingService } from '../data-sharing.service';
+import { DialogTemplateComponent, ModalType } from '../dialog/dialog-template/dialog-template.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-card',
@@ -8,16 +11,30 @@ import { IUser } from 'src/app/shared/interfaces/user';
   styleUrls: ['./user-card.component.css']
 })
 export class UserCardComponent implements OnInit {
-
   profile?: IUser;
-  constructor() { }
+
+  constructor(private dataSharingService: DataSharingService, private userService: UserService, private matDialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.getProfile();
+    let id = this.dataSharingService.getUserId();
+
+    if (id) {
+      this.getProfile(id);
+    } else {
+      let obj = { title: 'Error', message: "Something went wrong. Please sign in again", type: ModalType.WARN };
+      this.matDialog.open(DialogTemplateComponent, { data: obj });
+    }
   }
 
-  getProfile() {
-
+  getProfile(id: string) {
+    this.userService.getById(id).subscribe({
+      next: (res) => {
+        this.profile = res;
+      },
+      error: () => {
+        let obj = { title: 'Error', message: "Something went wrong", type: ModalType.WARN };
+        this.matDialog.open(DialogTemplateComponent, { data: obj });
+      }
+    })
   }
-
 }
